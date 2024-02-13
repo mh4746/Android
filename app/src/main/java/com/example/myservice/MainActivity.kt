3616101e70d76a98764c5f51621ba0a257b9d72e
root <mg474661@gmail.com>
2024-02-13 20:20:21 +0600

    Added text field

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -6,5 +6,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
+
 }
